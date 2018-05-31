9305f5fac354ccef5c40bab44a18d390cbad2986
qalanp <kumaritof@ya.ru>
2018-05-31 16:48:09 +0300

    add simple server-client

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 8080;
+const port = 1309;
 
-app.get('/', function(req, res) {
-    res.send('hello from server');
+server.listen(port);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('error', err);
-    }
-
-    console.log(`ok, port: ${port}`);
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
